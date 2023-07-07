35c5caffb50851a8b4bd192cca218ff881d29204
AnLan1214090013 <1214090013@qq.com>
2023-07-08 00:47:24 +0800

    Initial commit

---
+++ src/main/java/cn/pixelwar/pwlifesteal/PWLifeSteal.java
@@ -0,0 +1,17 @@
+package cn.pixelwar.pwlifesteal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PWLifeSteal extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/cn/pixelwar/pwlifesteal/PlayerStats/PlayerStat.java
@@ -0,0 +1,10 @@
+package cn.pixelwar.pwlifesteal.PlayerStats;
+
+public class PlayerStat {
+
+    private double hearts;
+    private double maxHearts;
+    private int kill;
+    private int death;
+
+}
