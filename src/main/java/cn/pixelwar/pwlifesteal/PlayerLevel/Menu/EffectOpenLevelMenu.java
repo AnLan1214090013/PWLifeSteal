8cdff2920a33acea18c12abe9cda75472600bbcc
AnLan1214090013 <1214090013@qq.com>
2023-08-20 22:25:23 +0800

    update

---
+++ src/main/java/cn/pixelwar/pwlifesteal/PlayerLevel/Menu/EffectOpenLevelMenu.java
@@ -0,0 +1,45 @@
+package cn.pixelwar.pwlifesteal.PlayerLevel.Menu;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import cn.pixelwar.pwlifesteal.Menu.SkillTreeMenu;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+
+import javax.annotation.Nullable;
+
+public class EffectOpenLevelMenu extends Effect {
+
+    private Expression<Player> playerin;
+    private Expression<Number> pagein;
+    static {
+        Skript.registerEffect(EffectOpenLevelMenu.class, new String[] {
+                "level %player% %number%",
+        });
+    }
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parser) {
+        this.playerin = (Expression<Player>) expressions[0];
+        this.pagein = (Expression<Number>) expressions[1];
+        return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        return "open level menu for player: ";
+    }
+
+    @Override
+    protected void execute(Event event) {
+        Player player = playerin.getSingle(event);
+        int page = pagein.getSingle(event).intValue();
+        LevelMenu levelMenu = new LevelMenu();
+        levelMenu.openLevelMenu(player, page);
+    }
+
+
+}
--- src/main/java/cn/pixelwar/pwlifesteal/PlayerLevel/Menu/LevelMenu.java
+++ src/main/java/cn/pixelwar/pwlifesteal/PlayerLevel/Menu/LevelMenu.java
@@ -1,5 +1,6 @@
 package cn.pixelwar.pwlifesteal.PlayerLevel.Menu;
 
+import cn.pixelwar.pwlifesteal.PlayerLevel.ServerLevelManager;
 import cn.pixelwar.pwlifesteal.PlayerStats.PlayerSkill.SkillType;
 import cn.pixelwar.pwlifesteal.PlayerStats.PlayerStatsManager;
 import cn.pixelwar.pwlifesteal.Utils.ChatColorCast;
@@ -19,115 +20,69 @@ import java.util.List;
 public class LevelMenu {
 
 
-    public void openLevelMenu(Player player){
-        Inventory gui = Bukkit.createInventory(player, 54, ChatColorCast.format("&8玩家等级 ("+PlayerStatsManager.playerStatMap.get(player.getName()).getRuby()));
-        for (SkillType skillType : SkillType.values()){
-            ItemStack item = getSkillItem(skillType, player);
-            gui.setItem(skillType.getMenuSlot(), item);
+    public void openLevelMenu(Player player, int page){
+        Inventory gui = Bukkit.createInventory(player, 54, ChatColorCast.format("&8玩家等级 ("+page+"/"+ (int)(ServerLevelManager.allLevels.size()/18+1)+")"));
+
+        int commonSlot[] = {9,10,11,12,13,14,15,16,17,   27,18,29,30,31,32,33,34,35};
+        int premiumSlot[] = {18,19,20,21,22,23,24,25,26,   36,37,38,39,40,41,42,43,44};
+        //第一页: 0 - 17
+        //第2页: 18 - 35
+        int count = 0;
+        for(int level = (page-1)*18; level<(page-1)*18+17;level++){
+
+            if (ServerLevelManager.allLevels.containsKey(level)){
+
+                    gui.setItem(commonSlot[count], getCommonItem(level, player));
+                    gui.setItem(commonSlot[count]+9, getPremiumItem(level, player));
+
+            }
+            count+=1;
         }
+
+        //第0级
+        ItemStack zero = new ItemStack(Material.LIME_STAINED_GLASS_PANE);
+        ItemMeta itemMeta1 = zero.getItemMeta();
+        itemMeta1.setDisplayName(ChatColorCast.format("&a&l等级 0"));
+        List<String> lore = new ArrayList<>();
+        lore.add("");
+        lore.add(ChatColorCast.format("&7此等级默认解锁"));
+        itemMeta1.setLore(lore);
+        zero.setItemMeta(itemMeta1);
+        gui.setItem(9, zero);
+        ItemStack zeroP = new ItemStack(Material.MINECART);
+        ItemMeta itemMeta2 = zeroP.getItemMeta();
+        itemMeta2.setDisplayName(ChatColorCast.format("&6&l等级 0额外奖励"));
+        List<String> lore2 = new ArrayList<>();
+        lore.add("");
+        itemMeta2.setLore(lore2);
+        zeroP.setItemMeta(itemMeta2);
+        gui.setItem(18, zeroP);
         player.openInventory(gui);
     }
 
-    public ItemStack getSkillItem(SkillType skillType, Player player){
+    public ItemStack getCommonItem(int level, Player player){
         NumberFormat numberFormat = new NumberFormat();
-        HashMap<SkillType, Integer> skillStat = PlayerStatsManager.playerStatMap.get(player.getName()).getSkillStat();
-        ItemStack item = new ItemStack(Material.GRAY_DYE);
+        ItemStack item = new ItemStack(Material.RED_STAINED_GLASS_PANE);
         ItemMeta itemMeta = item.getItemMeta();
-        //如果已经解锁过
-        if (skillStat.containsKey(skillType)){
-            //如果没达到最高等级
-            if (skillStat.get(skillType)<skillType.getMaxLevel()) {
-                String chance = numberFormat.getDoubleFormat(skillType.getDefaultChance() + (double) (skillStat.get(skillType) * skillType.getEachLevelChance())) ;
-                item = new ItemStack(Material.LIME_DYE);
-                String displayName = ChatColorCast.format("&a&l" + skillType.getDisplayName() + " &f(等级" + skillStat.get(skillType) + "/" + skillType.getMaxLevel() + ")");
-                List<String> lore = new ArrayList<>();
-                for (String s : skillType.getDescription()) {
-                    lore.add(s);
-                }
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&4&l| &f当前触发几率: &b" + chance + "%"));
-                lore.add(ChatColorCast.format("&4&l| &f下一级花费: &d" + (skillType.getUnlockPrize() + skillType.getEachLevelAddPrize() * skillStat.get(skillType)) + " 紫宝石"));
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&a▸ 点击升级"));
-                itemMeta.setDisplayName(displayName);
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-                NBTItem nbtItem = new NBTItem(item);
-                nbtItem.setString("skill", skillType.toString());
-                nbtItem.setInteger("cost", (skillType.getUnlockPrize() + skillType.getEachLevelAddPrize() * skillStat.get(skillType)));
-                item = nbtItem.getItem();
-            }else{
-                String chance = numberFormat.getDoubleFormat(skillType.getDefaultChance() + (double) (skillStat.get(skillType) * skillType.getEachLevelChance())) ;
-                item = new ItemStack(Material.ORANGE_DYE);
-                String displayName = ChatColorCast.format("&6&l" + skillType.getDisplayName() + " &f(等级" + skillStat.get(skillType) + "/" + skillType.getMaxLevel() + ")");
-                List<String> lore = new ArrayList<>();
-                for (String s : skillType.getDescription()) {
-                    lore.add(s);
-                }
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&4&l| &f当前触发几率: &b" + chance + "%"));
-//                lore.add(ChatColorCast.format("&4&l| &f下一级花费: &c" + (skillType.getUnlockPrize() + skillType.getEachLevelAddPrize() * skillStat.get(skillType)) + " 紫宝石"));
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&6已达到最高等级"));
-                itemMeta.setDisplayName(displayName);
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-            }
-        }
-        //如果还没有解锁
-        else{
-            //检查是否已经解锁连接的
-            boolean isUnlock = false;
-            for (String skillName : skillType.getConnectedSkills()){
-                SkillType connected = SkillType.getSkillType(skillName);
-                if (skillStat.containsKey(connected)){
-                    isUnlock = true;
-                }
-            }
-            //如果可以解锁
-            if (skillType.equals(SkillType.LESS_DAMAGE_FROM_MOB) || isUnlock){
-                String chance = numberFormat.getDoubleFormat(skillType.getDefaultChance()) ;
-                String displayName = ChatColorCast.format("&7&l"+skillType.getDisplayName()+" &f(等级0/"+skillType.getMaxLevel()+")");
-                List<String> lore = new ArrayList<>();
-                for (String s : skillType.getDescription()){
-                    lore.add(s);
-                }
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&4&l| &f当前触发几率: &b"+chance+"%"));
-                lore.add(ChatColorCast.format("&4&l| &f解锁花费: &d"+skillType.getUnlockPrize()+" 紫宝石"));
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&a▸ 点击解锁技能"));
-                itemMeta.setDisplayName(displayName);
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-                NBTItem nbtItem = new NBTItem(item);
-                nbtItem.setString("skill", skillType.toString());
-                nbtItem.setInteger("cost", skillType.getUnlockPrize());
-                item = nbtItem.getItem();
-            }
-            //如果不能解锁
-            else{
-                String chance = numberFormat.getDoubleFormat(skillType.getDefaultChance()) ;
-                String displayName = ChatColorCast.format("&7&l"+skillType.getDisplayName()+" &f(等级0/"+skillType.getMaxLevel()+")");
-                List<String> lore = new ArrayList<>();
-                for (String s : skillType.getDescription()){
-                    lore.add(s);
-                }
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&4&l| &f当前触发几率: &b"+chance+"%"));
-                lore.add(ChatColorCast.format("&4&l| &f解锁花费: &d"+skillType.getUnlockPrize()+" 紫宝石"));
-                lore.add(" ");
-                lore.add(ChatColorCast.format("&c&l✖ &c当前技能不能解锁,你需要"));
-                lore.add(ChatColorCast.format("&c先解锁与这个技能相连的技能"));
-                itemMeta.setDisplayName(displayName);
-                itemMeta.setLore(lore);
-                item.setItemMeta(itemMeta);
-            }
-        }
+        String displayName = ChatColorCast.format("&c&l等级" + level);
+        List<String> lore = new ArrayList<>();
+        itemMeta.setDisplayName(displayName);
+        itemMeta.setLore(lore);
+        item.setItemMeta(itemMeta);
 
         return item;
+    }
+    public ItemStack getPremiumItem(int level, Player player){
+        NumberFormat numberFormat = new NumberFormat();
+        ItemStack item = new ItemStack(Material.CHEST_MINECART);
+        ItemMeta itemMeta = item.getItemMeta();
+        String displayName = ChatColorCast.format("&6&l等级" + level+"额外奖励");
+        List<String> lore = new ArrayList<>();
+        itemMeta.setDisplayName(displayName);
+        itemMeta.setLore(lore);
+        item.setItemMeta(itemMeta);
 
-
+        return item;
     }
 
 
